f8faec7783f01e2d6aa3c28ab7c2d9f2411bf52a
Loic Thomas <l_thomas@insa-toulouse.fr>
2022-05-16 10:19:39 +0200

    signal réel

--- PjtKEIL_DFT_Signal_Reel/Src/principal.c
+++ PjtKEIL_DFT_Signal_Reel/Src/principal.c
@@ -6,28 +6,52 @@
 extern short int LeSignal;
 
 int tab[64];
+short dma_buf[64];
+
+void Systick_function()
+{
+	//démarrage de la DMA sur 64 échantillons:
+	Start_DMA1(64);
+	//attente de la fin de DMA:
+	Wait_On_End_Of_DMA1();
+	//•arrêter la DMA
+	Stop_DMA1;
+	
+	for (int i=0; i<64; i++){
+	tab[i] = DFT_ModuleAuCarre(dma_buf, i); 
+	}
+}
 
 int main(void)
 {
-
 // ===========================================================================
 // ============= INIT PERIPH (faites qu'une seule fois)  =====================
 // ===========================================================================
-
 	
-// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
-CLOCK_Configure();
-
-for (int i=0; i<64; i++){
-	tab[i] = DFT_ModuleAuCarre(&LeSignal, i); 
-}
-
+	// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+	CLOCK_Configure();
 	
+	//---CONFIGURATION DE SYSTICK---
+	//réglage de la périodicité du systick
+	Systick_Period_ff(360000);
+	//configuration du timer (débordement) 
+	Systick_Prio_IT( 2, Systick_function);
+	//lancer le systick
+	SysTick_On;
+	//valider   les   interruptions
+	SysTick_Enable_IT;
 	
-
+	//---CONFIGURATION DE TIM2---
+	//Activation de ADC1 avec prélèvement de 1us 
+	Init_TimingADC_ActiveADC_ff( ADC1, 72 );
+	//Choix du pin d'entrée
+	Single_Channel_ADC( ADC1, 2 );
+	//Configuration du timer 2 pour qu'ADC1 soit à 320 kHz
+	Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
+	//Configuration du remplissage de RAM
+	Init_ADC1_DMA1( 0, dma_buf );
 //============================================================================	
-	
-	
+		
 while	(1)
 	{
 	}
--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 BE CHTI LASER
 
 Travail effectué :
+DFT sur le vrai signal
+Test via la CibleSondeKeil, avec un offset de 1,65V; une amplitude de 3,3; des fréquences de 85, 90, 100kHz, en signal carré.
+On a les pics aux bonnes valeurs dans le tableau
+
+
+Dernière séance :
 Fin de la DFT 
 testé grâce au cosinus de fréquence 1 et un autre signal généré sur matlab (f = 3; déphasage = 25°) => pics de 255.99... à f = 3 et f = 61
 
-Travail séance prochaine :
-DFT sur le vrai signal
+
